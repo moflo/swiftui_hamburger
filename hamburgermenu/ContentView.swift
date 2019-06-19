820e4bbbc1ac626025be9564a3c542142298740a
admin <admin@demo.com>
2019-06-19 10:34:59 -0700

    initial

---
+++ hamburgermenu/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  hamburgermenu
+//
+//  Created by admin on 6/19/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
